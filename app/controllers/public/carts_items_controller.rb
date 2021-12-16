76990cdc82cfd8632115dbdba666d39f3abf6444
kotahorii <khorii.micron@gmail.com>
2021-12-16 05:21:11 +0000

    [Update] carts items controller

--- app/controllers/public/carts_items_controller.rb
+++ app/controllers/public/carts_items_controller.rb
@@ -1,19 +1,24 @@
 class Public::CartsItemsController < ApplicationController
+  before_action :set_carts_item, only: %i[update destroy]
   
   def index
-    
+    @carts_items = current_customer.carts_items.all
   end
   
   def create
+    @carts_item = current_customer.carts_items.new(carts_item_params) 
   end
   
   def update
   end
   
   def destroy
+    
   end
   
   def destroy_all
+    current_customer.carts_items.destroy_all
+    redirect_to carts_items_path
   end
   
   private
@@ -21,4 +26,7 @@ class Public::CartsItemsController < ApplicationController
   def carts_item_params
     params.require(:carts_item).permit(:item_id, :quantity)
   end
+  
+  def set_carts_item
+    @carts_item = CartsItem.find(params[:id])
 end
