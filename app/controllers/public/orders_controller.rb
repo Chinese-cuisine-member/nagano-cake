5542799ea4b2ea7cd498a9c63e72c5e1116059af
EC2 Default User <ec2-user@ip-172-31-86-126.ec2.internal>
2021-12-17 09:36:27 +0000

    order

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,17 +2,17 @@ class Public::OrdersController < ApplicationController
   def index
     @orders = current_customer.orders
   end
-  
+
   def new
     @order = Order.new
     @ships = Ship.where(customer: current_customer)
   end
-  
+
   def create
     @order = current_customer.orders.new(order_params)
     @order.save
     redirect_to orders_complete_path
-    
+
    @carts_items = current_customer.carts_items
    @carts_items.each do |carts_item|
      OrderDetail.create(
@@ -22,18 +22,24 @@ class Public::OrdersController < ApplicationController
        amount_price: (carts_item.item.price * 1.1).floor
        )
    end
-   
+
    @carts_items.destroy_all
   end
-  
+
   def show
     @order = Order.find(params[:id])
     @order_detail = OrderDetail.where(order_id: params[:id])
+
+    @total_payment = 0
+    @order_detail.item.each do |item|
+      @total_payment += item.subtotal
+    end
+    @amount_billed = @total_payment + @order.shipping_cost
   end
-  
+
   def confirm
     @carts_items = current_customer.carts_items
-    
+
     @order = Order.new(order_params)
     if params[:address_button] == 'my_address'
       @order.zipcode = current_customer.zipcode
@@ -44,21 +50,23 @@ class Public::OrdersController < ApplicationController
       @order.zipcode = @ship.zipcode
       @order.address = @ship.address
       @order.name = @ship.name
+    elsif params[:address_button] == 'new_address'
+      current_customer.address.(order_params)
     end
-    
+
     @total_payment = 0
     @carts_items.each do |carts_item|
       @total_payment += carts_item.subtotal
     end
     @amount_billed = @total_payment + @order.shipping_cost
   end
-  
+
   def complete
   end
-  
+
   private
   def order_params
     params.require(:order).permit(:zipcode, :address, :name, :payment_method, :payment)
   end
-  
+
 end
