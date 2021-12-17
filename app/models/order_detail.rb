2f456d2add53f831fa05c83ea73c9c1b60bb0487
kotahorii <khorii.micron@gmail.com>
2021-12-17 03:34:16 +0000

    [Update] ja.yml

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,5 +2,5 @@ class OrderDetail < ApplicationRecord
   belongs_to :item
   belongs_to :order
   
-  enum produciton_status: {"着手不可": 0,"製作待ち": 1,"製作中": 2,"製作完了": 3}
+  enum produciton_status: {cannot_make: 0, wait_for_making: 1, making: 2, complete: 3}
 end
