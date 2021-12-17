bec1d47546c9fe50aeb1bd75450f57f19a8c78eb
kotahorii <khorii.micron@gmail.com>
2021-12-17 08:06:53 +0000

    [Create] seed.rb and admin customers index.html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+20.times do |n|
+    Customer.create!(
+      email: "test#{n + 1}@test.com",
+      first_name: "名#{n + 1}",
+      last_name: "姓#{n + 1}",
+      first_name_kana: "セイ",
+      last_name_kana: "メイ",
+      password: "123456",
+      zipcode: "1234567",
+      address: "東京都渋谷区神南1丁目19-11 パークウェースクエア2 4階#{n + 1}",
+      phone_number: "09012345678",
+      is_deleted: false,
+    )
+end
+
+ Admin.create!(
+    email: "test@test.com",
+    password: "aaaaaa"
+  )
