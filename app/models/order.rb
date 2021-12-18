cf021528856f61ddc63a32892df700eee3a65be3
kotahorii <khorii.micron@gmail.com>
2021-12-18 04:07:00 +0000

    [Fix] show.html.erb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details, dependent: :destroy
-  
+
 
   enum payment_method: {credit_card: 0, transfer: 1}
 	enum status: {waiting_payment: 0, confirmed_payment: 1, making: 2, prepare_shipment: 3, shiped: 4}
